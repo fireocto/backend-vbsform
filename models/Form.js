b0a1f3fa01097196d78eba8e96699c3870ea5ab2
Jordan Powell <jordancolepowell@gmail.com>
2024-06-25 18:00:48 -0500

    Added routes and middleware still doesn't work right

---
+++ models/Form.js
@@ -0,0 +1,163 @@
+import mongoose from 'mongoose';
+
+const vbsFormSchema = new mongoose.Schema({
+    childName: {
+        type: String,
+        required: true,
+        validate: /^[A-Za-z ]*$/
+    },
+    childAge: {
+        type: Number,
+        required: true,
+        validate: /^[0-9]*$/
+    },
+    childAddress: {
+        type: String,
+        required: true,
+        validate: /^[A-Za-z0-9 ,]*$/
+    },
+    childInfo: {
+        type: String,
+        required: true,
+        validate: /^[A-Za-z0-9 ]*$/
+    },
+    childEmergencyContactName: {
+        type: String,
+        required: true,
+        validate: /^[A-Za-z0-9 ]*$/
+    },
+    childEmergencyContactNumber: {
+        type: String,
+        required: true,
+        validate: /^[0-9 -]*$/
+    },
+    childDismissal: {
+        type: String,
+        required: true,
+        validate: /^[A-Za-z0-9 ]*$/
+    },
+    guardianName: {
+        type: String,
+        required: true,
+        validate: /^[A-Za-z0-9 ]*$/
+    },
+    guardianNumber: {
+        type: String,
+        required: true,
+        validate: /^[0-9 ]*$/
+    },
+    guardianEmail: {
+        type: String,
+        required: true,
+        validate: /^[A-Za-z0-9@.]*$/  // Adjusted for email format, consider using a library or more robust validation for emails
+    },
+    guardianAddress: {
+        type: String,
+        required: true,
+        validate: /^[A-Za-z0-9 ]*$/
+    },
+    guardianChurch: {
+        type: String,
+        required: true,
+        validate: /^[A-Za-z0-9 ]*$/
+    },
+    guardianGuest: {
+        type: String,
+        required: true,
+        validate: /^[A-Za-z0-9 /]*$/
+    },
+    photoPermission: {
+        type: Boolean,
+        required: true
+    },
+});
+
+const Form = mongoose.model("Form", vbsFormSchema);
+
+export default Form;
+
+
+
+
+
+
+
+// import mongoose from 'mongoose'
+
+// const vbsFormSchema = new mongoose.Schema ({
+//     childName: {
+//         type: String,
+//         required: true,
+//         validate: /^[A-Za-z ]*$/
+//     },
+//     childAge: {
+//         type: String,
+//         required: true,
+//         validate: /^[0-18 ]*$/
+//     },
+//     childAddress: {
+//         type: String,
+//         required: true,
+//         validate: /^[A-Za-z0-9 ,]*$/
+//     },
+//     childInfo: {
+//         type: String,
+//         required: true,
+//         validate: /^[A-Za-z0-9 ]*$/
+//     },
+//     childEmergencyContactName: {
+//         type: String,
+//         required: true,
+//         validate: /^[A-Za-z0-9 ]*$/
+//     },
+//     childEmergencyContactNumber: {
+//         type: String,
+//         required: true,
+//         validate: /^[0-9 -]*$/
+//     },
+//     childDismissal: {
+//         type: String,
+//         required: true,
+//         validate: /^[A-Za-z0-9 ]*$/
+//     },
+//     guardianName: {
+//         type: String,
+//         required: true,
+//         validate: /^[A-Za-z0-9 ]*$/
+//     },
+//     guardianNumber: {
+//         type: String,
+//         required: true,
+//         validate: /^[0-9 ]*$/
+//     },
+//     guardianEmail: {
+//         type: String,
+//         required: true,
+//         validate: /^[A-Za-z0-9@ ]*$/
+//     },
+//     guardianAddress: {
+//         type: String,
+//         required: true,
+//         validate: /^[A-Za-z0-9 ]*$/
+//     },
+//     guardianChurch: {
+//         type: String,
+//         required: true,
+//         validate: /^[A-Za-z0-9 ]*$/
+//     },
+//     guardianGuest: {
+//         type: String,
+//         required: true,
+//         validate: /^[A-Za-z0-9 /]*$/
+//     },
+//     photoPermission: {
+//         type: Boolean,
+//         required: true,
+//         validate: /^[A-Za-z0-9 ]*$/
+//     },
+
+// })
+
+// const Form = mongoose.model("Form", vbsFormSchema)
+
+// export default Form
